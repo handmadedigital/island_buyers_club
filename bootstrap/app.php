3fc58f47baa673966192b0d733fbfb9ef2ad8fe5
rodrigo <lol@lol.lol>
2015-02-17 14:28:47 -0500

    update directory structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
 	'Illuminate\Contracts\Http\Kernel',
-	'App\Http\Kernel'
+	'TGL\Http\Kernel'
 );
 
 $app->singleton(
 	'Illuminate\Contracts\Console\Kernel',
-	'App\Console\Kernel'
+	'TGL\Console\Kernel'
 );
 
 $app->singleton(
 	'Illuminate\Contracts\Debug\ExceptionHandler',
-	'App\Exceptions\Handler'
+	'TGL\Exceptions\Handler'
 );
 
 /*
--- config/app.php
+++ config/app.php
@@ -139,11 +139,11 @@ return [
 		/*
 		 * Application Service Providers...
 		 */
-		'App\Providers\AppServiceProvider',
-		'App\Providers\BusServiceProvider',
-		'App\Providers\ConfigServiceProvider',
-		'App\Providers\EventServiceProvider',
-		'App\Providers\RouteServiceProvider',
+		'TGL\Providers\AppServiceProvider',
+		'TGL\Providers\BusServiceProvider',
+		'TGL\Providers\ConfigServiceProvider',
+		'TGL\Providers\EventServiceProvider',
+		'TGL\Providers\RouteServiceProvider',
 
 	],
 
