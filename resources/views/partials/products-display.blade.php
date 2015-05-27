7b65e34e9d8cd18a5a775b8d1405cbf620a9e226
rodrigo <rodrigo@threeaccents.com>
2015-05-27 01:39:39 -0400

    design fix

--- resources/views/partials/products-display.blade.php
+++ resources/views/partials/products-display.blade.php
@@ -37,41 +37,43 @@
                 </div>
             </div>
             <div id="productsDisplay">
-                <div class="row">
-                    @foreach($products as $product)
-                        <a href="/product/{{$product->slug}}">
-                            <div class="rod-5 col m4 s12  no-padding">
-                                <div class="product-display-wrapper">
-                                    <div class="product-display-image center">
-                                        <img src="/media/product_images/{{$product->images[0]->src}}">
-                                    </div>
-                                    <div class="product-display-info">
-                                        <div class="row">
-                                            <div class="col s8">
-                                                <h6>{{$product->name}}</h6>
+                @foreach(array_chunk($products->all(), 4) as $row)
+                    <div class="row">
+                        @foreach($row as $product)
+                            <a href="/product/{{$product->slug}}">
+                                <div class="rod-5 col m4 s12  no-padding">
+                                    <div class="product-display-wrapper">
+                                        <div class="product-display-image center">
+                                            <img src="/media/product_images/{{$product->images[0]->src}}">
+                                        </div>
+                                        <div class="product-display-info">
+                                            <div class="row">
+                                                <div class="col s8">
+                                                    <h6>{{$product->name}}</h6>
+                                                </div>
+                                                <div class="col s4">
+                                                    <h6>${{$product->masterVariant->price}}</h6>
+                                                </div>
                                             </div>
-                                            <div class="col s4">
-                                                <h6>${{$product->masterVariant->price}}</h6>
+                                            <div class="row">
+                                                <div class="col s12">
+                                                    <h6 class="product-display-categories">
+                                                        @foreach($product->categories as $category)
+                                                           {{$category->name}}
+                                                        @endforeach
+                                                    </h6>
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="row">
-                                            <div class="col s12">
-                                                <h6 class="product-display-categories">
-                                                    @foreach($product->categories as $category)
-                                                       {{$category->name}}
-                                                    @endforeach
-                                                </h6>
-                                            </div>
+                                        <div class="product-display-view">
+                                            <h6 class="center">View Product</h6>
                                         </div>
-                                    </div>
-                                    <div class="product-display-view">
-                                        <h6 class="center">View Product</h6>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                    @endforeach
-                </div>
+                            </a>
+                        @endforeach
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
