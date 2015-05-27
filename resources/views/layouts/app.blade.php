9cc18ccbb34844b224d85f4ccba046213bde3c8a
rodrigo <rodrigo@threeaccents.com>
2015-05-27 09:36:58 -0400

    design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,5 +12,47 @@
 <script src="/static/js/option-selector.js"></script>
 <script src="/static/js/add-product.js"></script>
 <script src="/static/js/sidebar.js"></script>
+<script>
+$(function()
+{
+    disableExtraOptions();
+    getOptionValues();
+});
+
+function getOptionValues()
+{
+    $('.select-option-values').change(function () {
+
+        var selected_options = getSelectedOptions();
+
+        $.get(
+            "/product/get-options",
+            {'selected_options[]': selected_options},
+            function(data){
+                $('#variantIdValue').val(data);
+            }
+        );
+    });
+}
+
+var disableExtraOptions = function () {
+
+    var select_box = $('.select-option-values');
+
+    select_box.attr("disabled", true);
+
+    select_box.first().attr("disabled", false);
+};
+
+function getSelectedOptions() {
+    var selected_options = [];
+    $('.select-option-values').each(function () {
+        if ($(this).is(":enabled")) {
+            selected_options.push($(this).val());
+        }
+    });
+    return selected_options;
+}
+</script>
 </body>
 </html>
